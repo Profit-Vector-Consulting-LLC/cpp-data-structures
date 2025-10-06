d11e896fb3719489492c96ae6c9c7829cb9fd6a4
EC2 Default User <ec2-user@ip-172-31-36-68.us-east-2.compute.internal>
2025-10-06 08:27:22 +0000

    Fixed comp8le errors on parenthisis example.  Added string reverse example.

---
+++ array/reverse.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+void reverse(char str[], int length) {
+   for(int i=0, j=length-1; i<j; i++,j--) {
+       char tmp = str[i];
+       str[i]=str[j];
+       str[j]=tmp;
+   }
+   str[length]=0;
+}
+
+
+int main() {
+   string text;
+
+   cout << endl << "Enter the text to reverse: ";
+   std::getline(cin, text);
+   char* buffer = NULL;
+   
+   try { 
+      buffer = new char[text.length()+1];
+      for(int i=0;i<text.length(); i++) {
+          buffer[i]=text.c_str()[i];
+      }
+      buffer[text.length()]=0;
+
+      reverse(buffer,text.length());
+      cout << "The reverse is: '" << buffer << "'" << endl;
+  }
+  catch(exception e) {
+      if( buffer != NULL ) {
+          delete buffer;
+      }
+      throw e;
+  }
+
+  cout << endl << endl;
+
+  return 0;
+}
--- stack/parens.cpp
+++ stack/parens.cpp
@@ -1,66 +1,62 @@
 #include <string>
 #include <stack>
-#include <stdexcept>
-#inlcude <iostream>
+#include <iostream>
 
 using namespace std;
 
-bool parseParen( char array[], int n) {
+bool parseParens( const char array[], int n) {
     
     // The variable formerly known as "aStack".  Naming is important
     // and clarifies the purpose - to track opening parens that need
     // to be resolved.
-    stack<char> openingParensToMatch  = new stack<char>();
-    
+    stack<char> openingParensToMatch;
+	    
     if( n == 0 ) {
         return true; // Degenerate case - string is empty, 
                      // thus no paren mismatches.
     }
     else if ( n<0 ) {
-       throw new Exception("Invalid state, parseParent expects n>=0");
+        throw new logic_error("Invalid state, parseParent expects n>=0");
     }
 
     int currentPos = 0;
     // Traverse the characters from left to right, finding 
     // each matching pair or quitting if we find a mismatch.
+
     do {
        // Grab the current character to process.
        char currentChar = array[currentPos];
        switch(currentChar) {
-          case '(': 
-                 
-		openingParensToMatch.push(currentChar);
-                break;
+           case '(': 
+               openingParensToMatch.push(currentChar);
+               break;
  
-          case ')':
-                if( aStack.size() > 0 ) {
-                   // We encountered a closing parenthesis with a 
-                   // matching open.  Clear the open paren from the stack.
-                   aStack.pop();
-                }
-                else {
-                   // We encountered a closing parenthesis without a 
-                   // matching opening one.  Once this happens there's
-                   // no way to recover, exit with answer of false.
-                   return false; 
-                }
-                break;
-	  default: 
-                throw new invalid_argument("Unexpected character " + currentChar  + found at position " + (currentPos+1) + "."); 
-       }
-       ++currentPos;
-    } while ( i < n );
+           case ')':
+              if( openingParensToMatch.size() > 0 ) {
+                  // We encountered a closing parenthesis with a 
+                  // matching open.  Clear the open paren from the stack.
+                  openingParensToMatch.pop();
+              }
+              else {
+                  // We encountered a closing parenthesis without a 
+                  // matching opening one.  Once this happens there's
+                  // no way to recover, exit with answer of false.
+                  return false; 
+              }
+           }
+           ++currentPos;
+        } while ( currentPos < n );
 
-    bool allParensResolved = openingParensToResolve.size()==0
-    return allParensResolved;
+        bool allParensResolved = openingParensToMatch.size()==0;
+        return allParensResolved;
 }
 
-int main void() {
+int main() {
 
-   String expr;
+   string expr;
 
    // Print an input propmt for the user.
-   cout << "Enter the expression: ";
+   cout << endl << "Enter the expression: ";
 
    // Accept the input from the keybord, store the result when 
    // user preses <enter> in the variable "expr".
@@ -71,30 +67,14 @@ int main void() {
    cout << endl;
 
 
-   // The "try" keyword tells us that we may throw an error that will
-   // break the normal flow of the program.  In this case if the user
-   // enters anything other than ( or ) and we try to process it, we'll
-   // throw a kind of error called an Exception. The code we'll test for the
-   // exception is inside the curly braces after the "try" keyword.  
-   // We can handle the error in the "catch" block (or code between curly braces
-   // that follows the "try" block.
-   try { 
-
-       // Test the input, then print the result to the screen.
-       if(parseParens(expr.c_str()) {
-           cout << "The expression has matching parenthesis." << endl;
-       } 
-       else { 
-           cout << "The expression has a parentheis mismatch." << endl;
-       }
+   // Test the input, then print the result to the screen.
+   if(parseParens(expr.c_str(), expr.length())) {
+       cout << "The expression has matching parenthesis." << endl;
+   } 
+   else { 
+       cout << "The expression has a parentheis mismatch." << endl;
    }
-   catch(exception e) {
-       //Whoops! User entered something unexpected! print the error message to the "standard error" output stream and quit with return code <> 0 (program encountered an error.
-       cerr << "ERROR: " << e.what() << endl;
-       return 1;
-   }    
 
-   // User provided valid input, return 0, meaning no error.
    return 0;
 }
 
