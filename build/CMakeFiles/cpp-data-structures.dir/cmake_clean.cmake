9c3bf561bdd844afd61f3ed5830022ea5d262e28
David Valko <davidvalko@COMPUTER.local>
2025-11-25 04:00:48 -0700

    Reworked projects to work with VSCode CMake Tools.

---
+++ build/CMakeFiles/cpp-data-structures.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-data-structures.dir/src/stack/parens.cpp.o"
+  "CMakeFiles/cpp-data-structures.dir/src/stack/parens.cpp.o.d"
+  "cpp-data-structures"
+  "cpp-data-structures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-data-structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cpp-data-structures.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp-data-structures.
+# This may be replaced when dependencies are built.
